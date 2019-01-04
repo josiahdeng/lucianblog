28cecda084e8b6014e6c07e34cbaae3edb5e8a3a
denglingxiang <17607003651@163.com>
2019-01-04 10:54:28 +0800

    提交后台代码实现demo

---
+++ source/fullcalendar/app/assets/javascripts/application.js
@@ -0,0 +1,24 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require moment.min
+//= require jquery-2.0.0.min
+//= require jquery-ui.min
+//= require rails-ujs
+// require activestorage
+// require turbolinks
+//= require fullcalendar
+// require locale-all
+//= require scheduler
+//= require theme-chooser
+// require zh-cn
+// require_tree .
---
+++ source/fullcalendar/app/controllers/full_controller.rb
@@ -0,0 +1,5 @@
+class FullController <  ApplicationController
+  def index
+
+  end
+end
